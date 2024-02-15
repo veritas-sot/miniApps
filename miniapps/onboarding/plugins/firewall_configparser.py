b87926e14c32daccf0b596cf8770d855be8dcc7c
veritas-sot <veritas-sot@proton.me>
2024-02-15 12:52:54 +0100

    configparser baded on abstract class
    configparser baded on abstract class

--- miniapps/onboarding/plugins/firewall_configparser.py
+++ miniapps/onboarding/plugins/firewall_configparser.py
@@ -2,16 +2,28 @@ from loguru import logger
 
 # veritas
 from veritas.onboarding import plugins
-from veritas.tools import tools
+from veritas.configparser import abstract_configparser
 
 
-class Firewall(object):
+class Firewall(abstract_configparser.Configparser):
 
-    def __init__(self, *unnamed, **named):
-        properties = tools.convert_arguments_to_properties(unnamed, named)
-        self._device_config = properties.get('config', None)
-        self._output_format = properties.get('output_format', 'json')
+    def __init__(self, config, platform, output_format, empty_config):
         logger.debug('initialized config parser for platform firewall')
+
+    def get_ipaddress(self, interface):
+        pass
+
+    def get_interface_name_by_address(self, address):
+        pass
+
+    def get_interface(self, interface):
+        pass
+
+    def find_in_global(self, properties):
+        pass
+
+    def find_in_interfaces(self, properties):
+        pass
 
 @plugins.configparser('firewall')
 def get_configparser(config, platform):
